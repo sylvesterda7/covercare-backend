1d795687b1f13e7fd86b28f21bfbcec58fecffbe
Sylvester Denyoh <sylvesterdenyoh7@gmail.com>
2026-06-20 12:50:58 +0000

    Fix PORT for Railway deployment

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const cors = require("cors");
 const puppeteer = require("puppeteer");
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
