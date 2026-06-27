3cd9845b8936fae9a024041b051061d17b11e75c
Sylvester Denyoh <sylvesterdenyoh7@gmail.com>
2026-06-27 21:00:50 +0000

    update BE

--- server.js
+++ server.js
@@ -20,7 +20,11 @@ app.use(cors({
   origin: [
     "https://covercare-africa.vercel.app",
     "http://localhost:5500",
-    "http://127.0.0.1:5500"
+    "http://127.0.0.1:5500",
+    "http://localhost:8000",
+    "http://127.0.0.1:8000",
+    "http://localhost:5501",
+    "http://127.0.0.1:5501"
   ],
   methods: ["GET", "POST"],
   allowedHeaders: ["Content-Type", "Authorization"]
